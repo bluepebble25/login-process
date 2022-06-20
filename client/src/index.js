d9c3b4fef0d1ee4d746677e9a4366e6c767c4161
bluepebble25 <bluepebble25@gmail.com>
2022-06-20 18:52:17 +0900

    add: antd, redux, middlewares for redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider
+        store={createStoreWithMiddleware(Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSIONS__ &&
+            window.__REDUX_DEVTOOLS_EXTENSIONS__()
+        )}>
+        <App />
+    </Provider>
 );
